6d818664aff1f224a4b4d1cf784b80d21bb8c0da
LAPTOP-N91UPOS7\dudtl <youngsik0266@gmail.com>
2021-06-03 16:31:49 +0900

    testsets

--- shop/src/main/java/shop/controller/ProductController.java
+++ shop/src/main/java/shop/controller/ProductController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import shop.model.ProductBean;
-import shop.model.ProductInput;
 import shop.service.ProductService;
 
 @Controller
